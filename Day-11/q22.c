79654676aeaf7647d6cb7ac95ac8f3c6fe48b4f9
Aaryan Gupta <aaryan.08.gupta@gmail.com>
2026-08-20 13:26:37 +0530

    Day 11: Month details and profit loss programs

---
+++ Day-11/q22.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    float cost_price, selling_price;
+    float profit, loss, percentage;
+
+    printf("Enter cost price: ");
+    scanf("%f", &cost_price);
+
+    printf("Enter selling price: ");
+    scanf("%f", &selling_price);
+
+    if (selling_price > cost_price)
+    {
+        profit = selling_price - cost_price;
+        percentage = (profit / cost_price) * 100;
+
+        printf("Profit = %.2f\n", profit);
+        printf("Profit Percentage = %.2f%%\n", percentage);
+    }
+    else if (cost_price > selling_price)
+    {
+        loss = cost_price - selling_price;
+        percentage = (loss / cost_price) * 100;
+
+        printf("Loss = %.2f\n", loss);
+        printf("Loss Percentage = %.2f%%\n", percentage);
+    }
+    else
+    {
+        printf("No Profit No Loss\n");
+    }
+
+    return 0;
+}
