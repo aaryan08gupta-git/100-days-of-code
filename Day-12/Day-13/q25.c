a1ded649828adf83166435df775537df8d997ce7
Aaryan Gupta <aaryan.08.gupta@gmail.com>
2026-08-23 22:21:14 +0530

    Day 13: Calculator and number printing programs

---
+++ Day-12/Day-13/q25.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter operator (+, -, *, /, %%): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    switch(op)
+    {
+        case '+':
+            printf("Answer = %d\n", a + b);
+            break;
+
+        case '-':
+            printf("Answer = %d\n", a - b);
+            break;
+
+        case '*':
+            printf("Answer = %d\n", a * b);
+            break;
+
+        case '/':
+            if (b != 0)
+                printf("Answer = %d\n", a / b);
+            else
+                printf("Cannot divide by zero\n");
+            break;
+
+        case '%':
+            if (b != 0)
+                printf("Answer = %d\n", a % b);
+            else
+                printf("Cannot divide by zero\n");
+            break;
+
+        default:
+            printf("Invalid operator\n");
+    }
+
+    return 0;
+}
