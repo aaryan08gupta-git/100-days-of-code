2a75d49a67d053d2d58648d2cd4bb21f0777926d
Aaryan Gupta <aaryan.08.gupta@gmail.com>
2026-09-08 01:03:31 +0530

    Day 30: Even odd and positive negative array programs

---
+++ Day-30/q59.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100];
+    int n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+
+    printf("Even numbers = %d\n", even);
+    printf("Odd numbers = %d\n", odd);
+
+    return 0;
+}
