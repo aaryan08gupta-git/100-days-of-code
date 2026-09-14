e578cb24a5fdabc198ad9ef3a49594347caf166e
Aaryan Gupta <aaryan.08.gupta@gmail.com>
2026-09-15 00:41:14 +0530

    Day 37: Row sum and transpose matrix programs

---
+++ Day-37/q73.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int sum[10];
+    int rows, cols, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++)
+    {
+        sum[i] = 0;
+
+        for (j = 0; j < cols; j++)
+        {
+            sum[i] = sum[i] + a[i][j];
+        }
+    }
+
+    printf("Row sums: ");
+
+    for (i = 0; i < rows; i++)
+    {
+        printf("%d ", sum[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
